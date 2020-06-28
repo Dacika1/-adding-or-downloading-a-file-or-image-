b910cabf8faaf7fc65b0a86702e2ec13d61aa9aa
Dacika1 <43290216+Dacika1@users.noreply.github.com>
2020-06-28 21:37:06 +0200

    Update web.php

--- web.php
+++ web.php
@@ -17,32 +17,8 @@ Route::get('/', function () {
 });
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/home2', 'HomeController@arhiva');
-Route::get('licni_podaci/{user}','FrstPageController@index2');
-Route::get('podaci','FrstPageController@create');
-Route::resource('licni_podaci','FrstPageController');
-Route::get('obrazovanje/nadji/{id}','educationController@nadji');
-Route::get('obrazovanje/kreiraj/{user}','educationController@kreiraj');
-Route::get('obrazovanje/prikaz/{user}','educationController@prikaz');
-Route::resource('obrazovanje','educationController');
-Route::get('zvanje/nadji/{id}','TitlesController@nadji');
-Route::get('zvanje/kreiraj/{user}','TitlesController@kreiraj');
-Route::get('zvanje/prikaz/{user}','TitlesController@prikaz');
-Route::resource('zvanje','TitlesController');
-Route::get('ugovori/nadji/{id}','WorkContractsController@nadji');
-Route::get('ugovori/kreiraj/{user}','WorkContractsController@kreiraj');
-Route::get('ugovori/prikaz/{user}','WorkContractsController@prikaz');
-Route::resource('ugovori','WorkContractsController');
-Route::get('drugo_angazovanje/nadji/{id}','NoneducatorExternalsController@nadji');
-Route::get('drugo_angazovanje/kreiraj/{work}','NoneducatorExternalsController@kreiraj');
-Route::get('drugo_angazovanje/prikaz/{work}','NoneducatorExternalsController@prikaz');
-Route::resource('drugo_angazovanje','NoneducatorExternalsController');
-Route::get('van_radnog_odnosa/nadji/{id}','ExternalContractsController@nadji');
-Route::get('van_radnog_odnosa/kreiraj/{user}','ExternalContractsController@kreiraj');
-Route::get('van_radnog_odnosa/prikaz/{user}','ExternalContractsController@prikaz');
-Route::post('van_radnog_odnosa2/zvanje','ExternalContractsController@steceno_zvanje');
-Route::resource('van_radnog_odnosa','ExternalContractsController');
+
+
 Route::get('skenirana_dokumenta/{url}', 'ScanedContoller@download');
 Route::get('skenirana_dokumenta','ScanedContoller@index');
 Route::get('sve/','ScanedContoller@tabela');
